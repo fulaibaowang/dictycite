55a161b0f989eaa256dd50fef17ffcc02cc053c7
Yun Wang <freedom_be@live.cn>
2026-04-30 10:38:32 +0200

    7a full test

--- scripts/private_scripts/hpc_scripts/vega/sbatch_pipeline_full_goldset.sh
+++ scripts/private_scripts/hpc_scripts/vega/sbatch_pipeline_full_goldset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J dicty_pipeline_7a_goldset
 #SBATCH -p gpu
-#SBATCH --time=48:00:00
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH -o logs/%x_%j.out
