fa637a0f9c3e402bb70f0c8c754c3a72c5e3ac3e
Yun Wang <freedom_be@live.cn>
2026-05-06 10:55:12 +0200

    reranker sweep with different CEs

--- scripts/private_scripts/hpc_scripts/frida/sbatch_frida_rerank_query_field_sweep_gemma.sh
+++ scripts/private_scripts/hpc_scripts/frida/sbatch_frida_rerank_query_field_sweep_gemma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J dicty_rerank_qf_7d_gemma
 #SBATCH -p frida
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=128G
 #SBATCH -o logs/%x_%j.out
